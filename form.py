454c478f4453f8e4cbe04b5c385f565bfb5ca2e2
yolevin <yolevin1@gmail.com>
2021-09-21 06:25:17 +0300

    Added the ability to distinguish between connection that occurred more then 5 minutes apart.

--- form.py
+++ form.py
@@ -1,30 +1,32 @@
 import time
 import os
+import math
+
+
 class form:
     def __init__(self):
         self.current_time = time.strftime("%H-%M-%S", time.localtime())
 
-        self.path = "data/data" + self.current_time+  ".txt"
-        f = open(self.path,'w+')
+        self.path = "data/data" + self.current_time + ".txt"
+        f = open(self.path, 'w+')
         f.close()
 
     def print2file(self, events):
-        if events == []:
+        if not events:
             return
 
         # data_file = "data.txt"
         # f = open(data_file, 'w')
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
-        str=current_time+";"
+        str = current_time + ";"
         for event in events:
-            str = str + " " + event+"$"
+            str = str + " " + event + "$"
 
         with open(self.path, "a") as f:
-            f.write(str+"\n")
+            f.write(str + "\n")
         print(str)
         f.close()
-
 
     def print_report(self):
         # f = open(self.path, 'r')
@@ -35,21 +37,27 @@ class form:
                 split1 = line.split(';')
                 split2 = split1[1]
                 split3 = split2.split('$')
-                split4= split3[:-1]
+                split4 = split3[:-1]
+                timestamp = split1[0]
                 for event in split4:
                     if event in dict.keys():
-                        dict[event]=dict[event]+1
+                        # Check timestamp to see if it is a new connection:
+                        if abs(int(timestamp.split(':')[1]) - int(dict[event][-1][1].split(':')[1])) <= 5:
+                            dict[event][-1][2] = dict[event][-1][2] + 1
+                            dict[event][-1][1] = timestamp
+                        else:
+                            dict[event].append([dict[event][-1][0] + 1, timestamp, 1])
                     else:
-                        dict[event]=1
+                        dict[event] = [[1, timestamp, 1]]  # [id, timestamp, count]
                 line = file.readline().rstrip()
-
-                    # print(split3)
+                # print(split3)
 
         current_time = time.strftime("%H-%M-%S", time.localtime())
         text = "report time: "+current_time+".\n\nSituations:\n"
 
         for t in dict:
-            text = text +  str(t[:-1])+":  "+str(dict.get(t))+" times.\n"
+            for con in dict[t]:
+                text = text + str(t[:-1]) + "(" + str(con[0]) + "):  "+str(con[2])+" times.\n"
 
         path = "forms/form" + current_time + ".txt"
         file = open(path, 'w+')
