6d447474dad2b58c1983dcf88cfdc424ab749644
yoavtamir <yoav.tamir@prospera.ag>
2023-06-10 10:02:31 +0300

    before moving into package

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
+import time
+
 import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 
 from categories_list import CategoriesList
+from configuration import Configuration
 from controller_client import ActionsTypes
+from controller_client import send_data
+from controller_server import run_server, accept_connection, process_data
 # from Camera import Camera
 from firebase_config_example import FirebaseConfig
 from form import form
 # initialize hand detector.
 from utils import speak, read_label_file
-from controller_server import run_server,accept_connection,process_data
-from controller_client import  send_data
-import time
+
 detector = HandDetector(detectionCon=0.6, maxHands=2)
 
 # creating objects
 firebase = FirebaseConfig()
 
 data_form = form()
-screen_width = 640
-screen_height = 480
 
 
 # my_camera = Camera(0, -10)
@@ -45,43 +46,9 @@ categories = CategoriesList(firebase, db)
 name = db.reference("name").get()
 
 first_person_show = False
-labels = read_label_file('Resources/coco.names.txt')
-
-# Extracting object names:
-classesFile = "Resources/coco.names.txt"
-with open(classesFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
-f.close()
-
-# Extracting pearson triggers:
-classesFile = "Resources/person.txt"
-with open(classesFile, 'rt') as f:
-    person_list = f.read().rstrip('\n').split('\n')
-f.close()
-
-# Extracting hand triggers:
-classesFile = "Resources/hand.txt"
-with open(classesFile, 'rt') as f:
-    hand_list = f.read().rstrip('\n').split('\n')
-f.close()
-
-classesFile = "Resources/other.txt"
-with open(classesFile, 'rt') as f:
-    other_list = f.read().rstrip('\n').split('\n')
-f.close()
-
-# initialize tensorflow object detector.
-# default_model_dir = 'Resources/ssd_mobilenet_v2_2'
-# default_model = 'ssd_mobilenet_v2.tflite'
-default_labels = 'coco_labels.txt'
 
 
-# initialize openCV and Camera settings
-# cap = cv2.VideoCapture(0)
-# screen_width = 640
-# screen_height = 360
-# cap.set(3, screen_width)
-# cap.set(4, screen_height)
+configuration = Configuration()
 
 
 def start_webcam():
@@ -216,7 +183,7 @@ def analyze_connections(connections):
 
 
 # analyze connections with hands. check if there is overlapping between objects and hands.
-def hand_connections(objs):
+def hand_connections(objs, hands, bboxes, classIds, possible_connections):
     for hand in hands:
         current_frame_connections = []
 
@@ -232,22 +199,22 @@ def hand_connections(objs):
             #             print(labels.get(obj_id,obj_id))
             #             if labels.get(obj_id,obj_id) == "bottle":
             #                 print("this")
-            if labels.get(obj_id, obj_id) in hand_list:
+            if configuration.labels.get(obj_id, obj_id) in configuration.hand_list:
                 if (x1 >= x2 + w2) or (x2 >= x1 + w1) or (y1 >= y2 + h2) or (y2 >= y1 + h1):
                     pass
                 else:
-                    if labels.get(obj_id, obj_id) in current_frame_connections:
+                    if configuration.labels.get(obj_id, obj_id) in current_frame_connections:
                         continue
-                    current_frame_connections.append(labels.get(obj_id, obj_id))
-                    if labels.get(obj_id, obj_id) in possible_connections:
-                        possible_connections[labels.get(obj_id, obj_id)] = \
-                            possible_connections[labels.get(obj_id, obj_id)] + 1
+                    current_frame_connections.append(configuration.labels.get(obj_id, obj_id))
+                    if configuration.labels.get(obj_id, obj_id) in possible_connections:
+                        possible_connections[configuration.labels.get(obj_id, obj_id)] = \
+                            possible_connections[configuration.labels.get(obj_id, obj_id)] + 1
                     else:
-                        possible_connections[labels.get(obj_id, obj_id)] = 1
+                        possible_connections[configuration.labels.get(obj_id, obj_id)] = 1
 
 
 # analyze connections with person. check if there is overlapping between objects and person.
-def person_connections(person_box: BBox):
+def person_connections(person_box: BBox, bboxes, classIds, possible_connections):
     if person_box is not None:
         current_frame_connections = []
         x2, y2, w2, h2 = [person_box.xmin, person_box.ymin, person_box.xmax, person_box.ymax]
@@ -256,19 +223,19 @@ def person_connections(person_box: BBox):
             box = bboxes[j]
             x1, y1, w1, h1, = box[0], box[1], box[2], box[3]
             obj_id = classIds[j]
-            if labels.get(obj_id, obj_id) in person_list:
+            if configuration.labels.get(obj_id, obj_id) in configuration.person_list:
                 if (x1 >= x2 + w2) or (x2 >= x1 + w1) or (y1 >= y2 + h2) or (y2 >= y1 + h1):
                     pass
                 else:
-                    if labels.get(obj_id, obj_id) in current_frame_connections:
+                    if configuration.labels.get(obj_id, obj_id) in current_frame_connections:
                         continue
                     #                     print(labels.get(obj_id,obj_id))
-                    current_frame_connections.append(labels.get(obj_id, obj_id))
-                    if labels.get(obj_id, obj_id) in possible_connections:
-                        possible_connections[labels.get(obj_id, obj_id)] = \
-                            possible_connections[labels.get(obj_id, obj_id)] + 1
+                    current_frame_connections.append(configuration.labels.get(obj_id, obj_id))
+                    if configuration.labels.get(obj_id, obj_id) in possible_connections:
+                        possible_connections[configuration.labels.get(obj_id, obj_id)] = \
+                            possible_connections[configuration.labels.get(obj_id, obj_id)] + 1
                     else:
-                        possible_connections[labels.get(obj_id, obj_id)] = 1
+                        possible_connections[configuration.labels.get(obj_id, obj_id)] = 1
 
 
 def get_objects(frame, threshold, model_loaded, img):
@@ -279,12 +246,8 @@ def get_objects(frame, threshold, model_loaded, img):
     indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
     draw_labels(boxes, colors, class_ids, classes, img, indexes)
 
-    #             x, y, w, h = boxes[i]
-    #             x, y, w, h = boxes[i]
-    # x_min, y_min, x_max, y_max = boxes[i]
-
     def make(bbox, score, id):
-        return Object(id, score, BBox(bbox[0], bbox[1], bbox[0]+ bbox[2], bbox[1]+bbox[3]))
+        return Object(id, score, BBox(bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]))
 
     if indexes is None:
         return []
@@ -303,7 +266,7 @@ def get_objects(frame, threshold, model_loaded, img):
 
 def camera_move_check(img, rectangle_size, center_object, person_box: BBox) -> ActionsTypes:
     half_rec_size = int(rectangle_size / 2)
-    center_screen = (int(screen_width / 2), int(screen_height / 2))
+    center_screen = (int(configuration.screen_width / 2), int(configuration.screen_height / 2))
     top_left = (center_screen[0] - half_rec_size, center_screen[1] - half_rec_size)
 
     cv2.rectangle(img, top_left, (top_left[0] + rectangle_size, top_left[1] + rectangle_size), (0, 0, 255), 2)
@@ -311,48 +274,23 @@ def camera_move_check(img, rectangle_size, center_object, person_box: BBox) -> A
     left_x = top_left[0]
     right_x = top_left[0] + rectangle_size
 
-    # top_box = person_box.ymin
-    # bottom_box = person_box.ymax
-    # threshold = 200
-    # if (top_box < top_left[1] + threshold) and (bottom_box > top_left[1] + rectangle_size - threshold):
-    #     return ActionsTypes.BACKWARD
-
     if x < left_x:
         return ActionsTypes.LEFT
 
     if x > right_x:
         return ActionsTypes.RIGHT
 
-    # if y< lower_y:
-    #     return (3, y-center_screen[1])
-    #
-    # if y> upper_y:
-    #     return (4, y-center_screen[1])
 
-
-def append_objs_to_img(cv2_image, objs):
-    height, width, channels = cv2_image.shape
-    # scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+def append_objs_to_img(cv2_image, objs, classIds, bboxes, hands, possible_connections):
     person_center = None
     person_box = None
     for obj in objs:
-        # percent = int(100 * obj.score)
         bbox = obj.bbox
-        # # bbox = obj.bbox.scale(scale_x, scale_y)
-        # # x0, y0 = int(bbox.xmin), int(bbox.ymin)
-        # # x1, y1 = int(bbox.xmax), int(bbox.ymax)
         x0, y0, x1, y1 = [bbox.xmin, bbox.ymin, bbox.xmax, bbox.ymax]
         bboxes.append([x0, y0, x1 - x0, y1 - y0])
         classIds.append(obj.id)
-        #
-        # label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
-        tag = labels.get(obj.id, obj.id)
-        #
-        # cv2_image = cv2.rectangle(cv2_image, (x0, y0), (x1, y1), (0, 255, 0), 2)
-        # cv2_image = cv2.putText(cv2_image, label, (x0, y0 + 30),
-        #                         cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+        tag = configuration.labels.get(obj.id, obj.id)
 
-        # in case object is person.
         if tag == 'person':
             person_box = bbox
             cv2.rectangle(cv2_image, (x0, y0), (x1, y1), (255, 0, 255), 2)
@@ -361,14 +299,18 @@ def append_objs_to_img(cv2_image, objs):
             cv2.circle(cv2_image, person_center, 10, (255, 0, 255), cv2.FILLED)
 
     # analyze connections with hands.
-    hand_connections(objs)
+    hand_connections(objs, hands, bboxes, classIds, possible_connections)
 
     # analyze connections with person.
-    person_connections(person_box)
+    person_connections(person_box, bboxes, classIds, possible_connections)
 
     return cv2_image, person_center, person_box
 
+
 def main():
+    model, classes, colors, output_layers = load_yolo()
+    model_loaded = [model, classes, colors, output_layers]
+    global first_person_show, move_direction
     print("waiting for power on to get started")
     while not firebase.is_on():
         time.sleep(2)
@@ -382,21 +324,16 @@ def main():
     print("capture started")
     firebase.initial()
     # TODO: robot should wake up and start to capture.
-    model, classes, colors, output_layers = load_yolo()
-    model_loaded = [model, classes, colors, output_layers]
 
     while True:
         classIds = []
-        confs = []
         possible_connections = {}
         connections = []
 
         if firebase.is_changed():
-            print(categories.get_importants())
             print("categories changed")
             categories.update_lists()
 
-            print(categories.get_importants())
             firebase.update_finished()
             print("categories update")
 
@@ -411,20 +348,20 @@ def main():
 
         is_person = False
 
-        for i in range(1):
+        for i in range(configuration.NUMBER_OF_ITERATIONS):
             bboxes = []
             frame = process_data()
             if frame is None:
                 continue
 
             cv2_im = frame.copy()
-            inference_size = (320, 320)
 
             threshold = 0.3
-            objs = get_objects(frame, threshold, model_loaded, cv2_im)
+            objs = get_objects(frame, threshold, model_loaded, cv2_im, )
 
             hands, cv2_im = detector.findHands(cv2_im)
-            cv2_im, person_center, person_box = append_objs_to_img(cv2_im, objs)
+            cv2_im, person_center, person_box = append_objs_to_img(cv2_im, objs, classIds, bboxes, hands,
+                                                                   possible_connections)
             is_person = person_center is not None or is_person
             if first_person_show == False and person_center != None:
                 first_person_show = True
@@ -442,7 +379,6 @@ def main():
                     send_data(move_direction)
 
             cv2.imshow("Image", cv2_im)
-        confidence_level = 0
         for key in possible_connections.keys():
             connections.append(key)
 
@@ -453,6 +389,7 @@ def main():
             send_data(ActionsTypes.START)
 
     print("capture stopped")
+
 
 if __name__ == '__main__':
     main()
