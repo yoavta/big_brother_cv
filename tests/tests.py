9ed728d463afb470c2aa46f799704b1345d036b0
yoavtamir <yoav.tamir@prospera.ag>
2023-06-10 15:15:55 +0300

    finish 3 tests:

--- tests/tests.py
+++ tests/tests.py
@@ -4,13 +4,14 @@ import cv2
 import pytest
 
 from src.DetectorStore import DetectorStore
-from src.main import camera_move_check
+from src.utils import camera_move_check
 
-models_store = DetectorStore()
+detectors_store = DetectorStore()
 root_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(root_dir, "test_images")
+image_path1 = os.path.join(root_dir, "test_images_1")
+image_path2 = os.path.join(root_dir, "test_images_2")
 
-is_debug = False
+is_debug = True
 
 
 @pytest.fixture(autouse=True)
@@ -21,11 +22,10 @@ def setup_and_teardown():
     print("Teardown")
 
 
-@pytest.mark.parametrize("image_path", [f'test_images/{img}' for img in os.listdir(image_path)])
+@pytest.mark.parametrize("image_path", [f'test_images_1/{img}' for img in os.listdir(image_path1)])
 def test_detect_hands(image_path):
     split_path = image_path.split('/')[1].split('.')[0].split('-')
     image_path = os.path.join(root_dir, image_path)
-
 
     if is_debug:
         cv2.imshow('img', cv2.imread(image_path))
@@ -33,7 +33,7 @@ def test_detect_hands(image_path):
 
     img = cv2.imread(image_path)
 
-    hands, img_res = models_store.hand_detector.get_model().findHands(img)
+    hands, img_res = detectors_store.hand_detector.get_model().findHands(img)
 
     if is_debug:
         cv2.imshow('img', img_res)
@@ -44,9 +44,8 @@ def test_detect_hands(image_path):
         hands) == num_of_expected_hands, f"Expected {num_of_expected_hands} hands, but found {len(hands)} in {image_path}"
 
 
-@pytest.mark.parametrize("image_path", [f'test_images/{img}' for img in os.listdir(image_path)])
+@pytest.mark.parametrize("image_path", [f'test_images_1/{img}' for img in os.listdir(image_path1)])
 def test_detect_people(image_path):
-
     split_path = image_path.split('/')[1].split('.')[0].split('-')
 
     if is_debug:
@@ -54,7 +53,7 @@ def test_detect_people(image_path):
         cv2.waitKey(0)
 
     img = cv2.imread(image_path)
-    blob, outputs = models_store.object_detector.detect_objects(img)
+    blob, outputs = detectors_store.object_detector.detect_objects(img)
 
     if is_debug:
         cv2.imshow('img', img)
@@ -66,13 +65,27 @@ def test_detect_people(image_path):
 
 
 # test if the move is correct
-@pytest.mark.parametrize("image_path", [f'test_images/{img}' for img in os.listdir(image_path)])
+@pytest.mark.parametrize("image_path", [f'test_images_2/{img}' for img in os.listdir(image_path2)])
 def test_detect_move(image_path):
     split_path = image_path.split('/')[1].split('.')[0].split('-')
     image_path = os.path.join(root_dir, image_path)
-    img = cv2.imread(image_path)
+    frame = cv2.imread(image_path)
+    cv2_im = frame.copy()
+    image_height, image_width, _ = frame.shape
+    threshold = 0.5
+    objs = detectors_store.object_detector.get_objects(frame, threshold, detectors_store.object_detector, cv2_im)
+    hands = []
+    bboxes = []
+    class_ids = []
+    possible_connections = {}
+    cv2_im, person_center, person_box = detectors_store.append_objs_to_img(cv2_im, objs, class_ids, bboxes, hands,
+                                                                           possible_connections)
+
     rectangle_size = 100
-
-
-
-    camera_move_check(img,rectangle_size,)
+    res = camera_move_check(cv2_im, rectangle_size, person_center, person_box,image_width,image_height)
+    if is_debug:
+        img_with_title = cv2.putText(cv2_im, str(res), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow('img', img_with_title)
+        cv2.waitKey(0)
+    first_letter = 'N' if not res else str(res).split('.')[1][0]
+    assert first_letter == split_path[0], f"Expected {split_path[0]} but got {first_letter}"
